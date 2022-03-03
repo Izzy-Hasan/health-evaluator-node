ed2bae7bc83296a230390b584670add4d5e50aeb
Katie Knutson <71470339+katherynmk@users.noreply.github.com>
2022-03-03 16:19:28 -0600

    instructions

--- index.js
+++ index.js
@@ -11,16 +11,30 @@ router.get('/',function(req,res){
 app.get('/calculate', (request, response) => {
 	console.log('Calling "/calculate" on the Node.js server.')
 	var inputs = url.parse(request.url, true).query
+	//everything up here  ^ leave it be
+
+	//these are the ints of the values that we assigned them in the html page
+	//also leave these alone, but you need to use them
 	let age = parseInt(inputs.age)
 	let height = parseInt(inputs.height)
-  let weight = parseInt(inputs.weight)
-  let blood = parseInt(inputs.blood)
-  let family = parseInt(inputs.family)
+    let weight = parseInt(inputs.weight)
+    let blood = parseInt(inputs.blood)
+  	let family = parseInt(inputs.family)
+
+	//calulations go here!
 
 
 
+
+
+
+	//sum should be returning a string of the final conseses of the health
+	//of the user
+	//right now its just a dummy calulation for display
 	let sum = age+height+weight+blood+family
-  console.log("calculate ran")
+  	
+	//leave below alone
+	console.log("calculate ran")
 	response.type('text/plain')
 	response.send(sum.toString())
 })
