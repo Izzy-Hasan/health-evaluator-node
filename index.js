b4293ead52abad8baf3b139a3f05a05445b1091c
katherynmk <katherynmknutson@lewisu.edu>
2022-03-03 15:36:36 -0600

    lookin gooooood

--- index.js
+++ index.js
@@ -8,7 +8,21 @@ router.get('/',function(req,res){
   //__dirname : It will resolve to your project folder.
 });
 
+app.get('/add-two-integers', (request, response) => {
+	console.log('Calling "/add-two-integers" on the Node.js server.')
+	var inputs = url.parse(request.url, true).query
+	let age = (inputs.age)
+	let height = (inputs.height)
+  let weight = (inputs.weight)
+  let blood = (inputs.blood)
+  let family = (inputs.family)
 
+
+
+	let sum = x + y
+	response.type('text/plain')
+	response.send(sum.toString())
+})
 
 
 app.use('/', router);
