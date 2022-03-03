7a29e2304d083db76c41e2be08c1d49ca2778dc1
katherynmk <katherynmknutson@lewisu.edu>
2022-03-03 15:20:40 -0600

    drop down

--- index.js
+++ index.js
@@ -8,54 +8,7 @@ router.get('/',function(req,res){
   //__dirname : It will resolve to your project folder.
 });
 
-app.get("/getall", function (request, response){
-  var message1 = request.query.message1;
 
-  if (message1 <= 100 && message1 > 0) {
-      response.send("thank you");
-  } else {
-      response.send("Please provide an appropriate age. ");
-  }
-});
-app.get("/getall", function (request, response){
-  var message2 = request.query.message2;
-
-  if (message2 <= 100 && message2 > 0) {
-      response.send("thank you");
-  } else {
-      response.send("Please provide an appropriate age. ");
-  }
-});
-
-app.get("/getall", function (request, response){
-  var message3 = request.query.message3;
-
-  if (message3 <= 100 && message3 > 0) {
-      response.send("thank you");
-  } else {
-      response.send("Please provide an appropriate age. ");
-  }
-});
-
-app.get("/getall", function (request, response){
-  var message4 = request.query.message4;
-
-  if (message4 <= 100 && message4 > 0) {
-      response.send("thank you");
-  } else {
-      response.send("Please provide an appropriate age. ");
-  }
-});
-
-app.get("/getall", function (request, response){
-  var message5 = request.query.message5;
-
-  if (message5 <= 100 && message5 > 0) {
-      response.send("thank you");
-  } else {
-      response.send("Please provide an appropriate age. ");
-  }
-});
 
 
 app.use('/', router);
