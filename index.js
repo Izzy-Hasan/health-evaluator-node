227734c305e32a540d2872e1459e797fa86254f3
katherynmk <katherynmknutson@lewisu.edu>
2022-03-03 15:59:17 -0600

    IT FREAKING WORKS IM SO SMART

--- index.js
+++ index.js
@@ -2,24 +2,25 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const router = express.Router();
-
+var url = require('url');
 router.get('/',function(req,res){
   res.sendFile(path.join(__dirname+'/index.html'));
   //__dirname : It will resolve to your project folder.
 });
 
 app.get('/calculate', (request, response) => {
-	console.log('Calling "/add-two-integers" on the Node.js server.')
+	console.log('Calling "/calculate" on the Node.js server.')
 	var inputs = url.parse(request.url, true).query
-	let age = (inputs.age)
-	let height = (inputs.height)
-  let weight = (inputs.weight)
-  let blood = (inputs.blood)
-  let family = (inputs.family)
+	let age = parseInt(inputs.age)
+	let height = parseInt(inputs.height)
+  let weight = parseInt(inputs.weight)
+  let blood = parseInt(inputs.blood)
+  let family = parseInt(inputs.family)
 
 
 
-	let sum = x + y
+	let sum = age+height+weight+blood+family
+  console.log("calculate ran")
 	response.type('text/plain')
 	response.send(sum.toString())
 })
