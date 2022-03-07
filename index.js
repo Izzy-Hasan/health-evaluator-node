f2729f52b1443bf35b5efd024dd4b795f83fa277
katherynmk <katherynmknutson@lewisu.edu>
2022-03-07 02:23:07 -0600

    calculations done and its prettier :)

--- index.js
+++ index.js
@@ -20,23 +20,174 @@ app.get('/calculate', (request, response) => {
     let weight = parseInt(inputs.weight)
     let blood = parseInt(inputs.blood)
   	let family = parseInt(inputs.family)
+	let bmi;
+	console.log("init height ", height)
+	console.log("init weight ", weight)
+	  if (height == 1) {
+		if (weight == 1){
+			//normal
+			bmi = 0;
+		}
+		if (weight == 2){
+			//normal
+			bmi = 0;
+		}
+		if (weight == 3){
+			//overweight
+			bmi = 30;
+		}
+		if (weight == 4){
+			//obese
+			bmi = 75;
+		}
+		if (weight == 5){
+			//obese
+			bmi = 75;
+		}
+		if (weight == 6){
+			//obese
+			bmi = 75;
+			
+		}
+	  }
 
-	//calulations go here!
+	  if (height == 2) {
+		if (weight == 1){
+			//underweight
+			bmi = 75;
 
+		}
+		if (weight ==2){
+			//healthy
+			bmi = 0;
+		}
+		if (weight == 3){
+			//healthy
+			bmi = 0;
+		}
+		if (weight == 4){
+			//overweight
+			bmi = 30;
+		}
+		if (weight == 5){
+			//overweight
+			bmi = 30;
+		}
+		if (weight == 6){
+			//obese
+			bmi = 75;
+		}
+	  }
 
+	  if (height == 3) {
+		if (weight == 1){
+			//underweight
+			bmi = 75;
+		}
+		if (weight == 2){
+			//healthy
+			bmi = 0;
+		}
+		if (weight == 3){
+			//healthy
+			bmi = 0;
+		}
+		if (weight == 4){
+			//healthy
+			bmi = 0;
+		}
+		if (weight == 5){
+			//overweight
+			bmi = 30;
+		}
+		if (weight == 6){
+			//obese
+			bmi = 75;
+		}
+	  }
 
+	  if (height == 4) {
+		if (weight ==1){
+			//underweight
+			bmi = 75;
+		}
+		if (weight == 2){
+			//underweight
+			bmi = 75;
+		}
+		if (weight == 3){
+			//healthy
+			bmi = 0;
+		}
+		if (weight == 4){
+			//healthy
+			bmi = 0;
+		}
+		if (weight == 5){
+			//overweight
+			bmi = 30;
+		}
+		if (weight == 6){
+			//overweight
+			bmi = 30;
+		}
+	  }
 
+	  if (height == 5) {
+		if (weight == 1){
+			//underweight
+			bmi = 75;
+		}
+		if (weight == 2){
+			//underweight
+			bmi = 75;
+		}
+		if (weight == 3){
+			//underweight
+			bmi = 75;
+		}
+		if (weight == 4){
+			//healthy
+			bmi = 0;
+		}
+		if (weight == 5){
+			//healthy 
+			bmi = 0;
+		}
+		if (weight == 6){
+			//overweight
+			bmi = 30
+		}
+	  }
 
+	    let risk = "Your Calculated Risk Category is ";
+		let sum = age+blood+family+bmi
+         console.log("sum ", sum)
 
-	//sum should be returning a string of the final conseses of the health
-	//of the user
-	//right now its just a dummy calulation for display
-	let sum = age+height+weight+blood+family
-  	
+  	  if(sum <= 20){
+		risk = risk +"low risk"
+	  }
+	  else if(sum <= 50){
+		risk = risk + "moderate risk"
+	  }
+	  else if(sum <= 75){
+		risk = risk + "high risk"
+	  }
+	  else{
+		risk = risk + "uninsurable"
+	  }
+		console.log("risk= ", risk)
+
+	  console.log("age ",age)
+	  console.log("height ",height)
+	  console.log("weight ",weight)
+	  console.log("bmi ",bmi)
+	  console.log("blood ",blood)
+	  console.log("family ",family)
 	//leave below alone
 	console.log("calculate ran")
 	response.type('text/plain')
-	response.send(sum.toString())
+	response.send(risk.toString())
 })
 
 
